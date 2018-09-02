c640caa61f6feeca878b5b216f9ac38c41fe17f0
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-01 20:32:32 -0400

    Initial commit

---
+++ DemoProjectSeleniumModified/TestCases/TestClass.cs
@@ -0,0 +1,78 @@
+﻿using DemoProjectSeleniumModified.POM;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProjectSeleniumModified.TestCases
+{
+    class TestClass
+    {
+        [SetUp]
+        public void Initialize()
+        {
+            PropertiesCollection.driver = new ChromeDriver();
+            //navigate to automationpractice.com
+            PropertiesCollection.driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            //maximize window
+            PropertiesCollection.driver.Manage().Window.Maximize();
+
+            //Find the sign-in text box element
+            PropertiesCollection.driver.FindElement(By.ClassName("login")).Click();
+
+            Console.WriteLine("Opened URL and clicked Login Page");
+
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+
+            //login to application by creating an instance of Authentication class
+            Authentication authentication = new Authentication();
+
+
+            AccountPage accountPage = authentication.SignIn("asangeethu@yahoo.com", "@nuK1978");
+            SearchResultsPage searchResults = accountPage.EnterSearchText("printed summer dress");
+
+            Console.WriteLine("Executed Login page and did a search query in the next POM");
+
+            searchResults.AddToCart();
+            Console.WriteLine("Hover on search result and added item to cart");
+
+
+        }
+
+
+        [Test]
+        public void IncorrectUserIdOrPassword()
+        {
+            //login to application by creating an instance of Authentication class
+            Authentication authentication = new Authentication();
+            AccountPage accountPage = authentication.SignIn("asanhu@yahoo.com", "qwertop8");
+            //AccountPage accountPage = authentication.SignIn("asangeethu@yahoo.com", "@nuK1978");
+
+            IWebElement loginMessage = PropertiesCollection.driver.FindElement(By.XPath("//*[@id=\"center_column\"]/div[1]/ol/li"));
+            Console.WriteLine(loginMessage.Text);
+            Boolean loginError = loginMessage.Text.Contains("Authentication failed");
+            Console.WriteLine(loginError);
+            Assert.IsFalse(loginError);
+        }
+
+
+
+
+        [TearDown]
+        public void CleanUp()
+        {
+            //close window
+            //driver.Close();
+
+            Console.WriteLine("Closed the browser");
+        }
+    }
+}
